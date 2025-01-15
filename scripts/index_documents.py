061afc625244b58615a6e5eaba1196e5bc796562
ciaograsso06 <cmmsilva@timbrasil.com.br>
2025-01-15 15:04:29 -0300

    Changing the methods

--- scripts/index_documents.py
+++ scripts/index_documents.py
@@ -1,23 +1,34 @@
 import os
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader
 
-EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
-
-DOCUMENTS_DIR = "../data/documents"
-INDEX_DIR = "../data/index"
+DOCUMENT_PATH = "../data/index/documentation/zabbix_documentation.pdf"
+VECTORSTORE_PATH = "../data/index"
 
 def index_documents():
-    embed_model = HuggingFaceEmbedding(model_name=EMBEDDING_MODEL_NAME)
+    
+    print(f"Carregando documentação de {DOCUMENT_PATH}...")
+    loader = PyPDFLoader(document_path=DOCUMENT_PATH)
+    documents = loader.load()
+    
+    print("Dividindo o texto em chunks...")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=50)
+    docs = text_splitter.split_documents(documents)
+    
+    print("Carregando embeddings...")
+    
+    embeddings = HuggingFaceBgeEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+    
+    print("Indexando documentos...")
+    texts = [doc.page_content for doc in docs]
+    vectorstore = FAISS.from_texts(texts, embeddings)
+    
+    os.makedirs(os.path.dirname(VECTORSTORE_PATH), exist_ok=True)
+    vectorstore.save_local(VECTORSTORE_PATH)
+    print(f"Indice salve em {VECTORSTORE_PATH}")
 
-    documents = SimpleDirectoryReader(input_dir=DOCUMENTS_DIR).load_data()
-
-    index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
-
-    os.makedirs(INDEX_DIR, exist_ok=True)
-    index.storage_context.persist(persist_dir=INDEX_DIR)
-
-    print(f"Índice criado e salvo em: {INDEX_DIR}")
 
 if __name__ == "__main__":
     index_documents()
